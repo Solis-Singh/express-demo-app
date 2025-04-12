f2a2be8a6fa404790d43dd5c3598249d3fb12f8e
SolisSingh <solissingh@gmail.com>
2025-04-12 12:52:24 +0530

    Third Commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 import User from '../models/userModel.js'
 
+
+     //Post Method   ( adding a new user to the database )
 export const addNewUser = async(req , res) => {
     const user = new User(req.body);
     console.log(req.body);
@@ -7,3 +9,42 @@ export const addNewUser = async(req , res) => {
 
     res.send(user);
 }
+
+        //Get Method  ( Getting user in database )
+export const getUser = async(req , res) => {    
+    const data = await User.find();
+    res.send(data);
+}
+
+export const getUserById = async(req , res) => {
+    const id = req.params._id;
+    const data = await User.find({_id : id});
+    res.status(200).send(data);
+}
+
+    // Put Method ( Updating User in Database)
+    export const updateUser = async(req, res) => {
+        try{
+            let userData = await User.findByIdAndUpdate(req.params._id, req.body,{new :true});
+            console.log(userData);
+            res.send(userData);
+        }catch (err){
+            res.send(err);
+        }
+        
+    };
+    
+
+        //Delete Method (Deleting User from database)
+    export const deleteUserById = async (req,res) => {
+        try {
+            let userData = await User.findByIdAndDelete(req.params._id,{new : true});
+            console.log("Gettting Data : ", userData);
+
+        res.send(userData)
+        } catch {
+            res.send(err);
+        }
+        
+    };    
+            
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 // import { getUser,getUserById,postUser,putUser,deleteUser } from '../Logic/userLogic.js';
-import {addNewUser} from '../controller/userController.js'
+import {addNewUser ,deleteUserById,getUser, getUserById, updateUser} from '../controller/userController.js'
 const router = express.Router()
 
-// router.get("/user", getUser );
+router.get("/users", getUser );
 
 
 //         //get method
-// router.get("/user/:id", getUserById);
+router.get("/user/:_id", getUserById);
 
 //         //post method (create a new user)
 
@@ -15,10 +15,10 @@ router.post("/user", addNewUser);
 
 //     //put method (update user)
 
-// router.put("/user/:id", putUser);
+router.put("/user/:_id", updateUser);
 
 //     //delete method ( deleting user from array)
-// router.delete("/user/:id/delete", deleteUser);
+router.delete("/user/:_id", deleteUserById);
 
 
 export default router;
